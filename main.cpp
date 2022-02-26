b4cded8e008683c677420cea2ac8f119ba041ff3
Pascal Klaßen <p.klassen221199@gmail.com>
2022-02-26 20:12:03 +0100

    Setting up a Test Framework for OpenGL

--- main.cpp
+++ main.cpp
@@ -7,18 +7,12 @@
 #include <imgui_impl_opengl3.h>
 
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <sstream>
 
 #include <malloc.h>
 
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
-#include "Shader.h"
-#include "Texture.h"
 #include "Renderer.h"
+#include "TestClearColor.h"
 
 int main()
 {
@@ -52,48 +46,6 @@ int main()
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-        GLfloat vertices[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,
-             50.0f, -50.0f, 1.0f, 0.0f,
-             50.0f,  50.0f, 1.0f, 1.0f,
-            -50.0f,  50.0f, 0.0f, 1.0f
-        };
-
-        GLuint indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        VertexBuffer vbo{ vertices, 4 * 4 * sizeof(GLfloat) };
-
-        VertexBufferLayout layout;
-        layout.Push<GLfloat>(2);
-        layout.Push<GLfloat>(2);
-
-        VertexArray vao;
-        vao.AddBuffer(vbo, layout);
-
-        IndexBuffer ibo{ indices, 6 };
-
-        Shader shader{ "Basic.shader" };
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-        glm::mat4 proj = glm::ortho(00.f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4{ 1.0f }, glm::vec3{ 0, 0, 0 });
-
-        Texture texture{ "opengl-logo.png" };
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        vao.Unbind();
-        vbo.Unbind();
-        ibo.Unbind();
-        shader.Unbind();
-
         Renderer renderer;
 
         ImGui::CreateContext();
@@ -102,51 +54,19 @@ int main()
 
         ImGui::StyleColorsDark();
 
-        glm::vec3 translationA{ 200, 200, 0 };
-        glm::vec3 translationB{ 400, 200, 0 };
+        test::TestClearColor test;
 
-        GLfloat r = 0.0f;
-        GLfloat inc = 0.05f;
         while (!glfwWindowShouldClose(window))
         {
             renderer.Clear();
 
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4{ 1.0f }, translationA);
-                glm::mat4 mvp = proj * view * model;
-
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(vao, ibo, shader);
-            }
-            
-            {
-                glm::mat4 model = glm::translate(glm::mat4{ 1.0f }, translationB);
-                glm::mat4 mvp = proj * view * model;
-
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(vao, ibo, shader);
-            }
-
-            if (r > 1.0f)
-                inc = -0.05f;
-            else if (r < 0.0f)
-                inc = 0.05f;
-
-            r += inc;
-
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
